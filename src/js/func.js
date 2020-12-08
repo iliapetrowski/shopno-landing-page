ffc9a3495118bdf1d73e51f5eb4068f41d6c09d8
ilia <ilia.petrowski@gmail.com>
2020-12-08 21:38:16 +0300

    add header image

--- src/js/func.js
+++ src/js/func.js
@@ -1,3 +1,1 @@
-export function asd() {
-	console.log('worked')
-}
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
-import {asd} from './func';
 
-asd();
+
+const images = document.querySelectorAll('.works__img-item')
+images.forEach(item => item.addEventListener('click', openModalWindow))
+
+
+function openModalWindow(){
+    console.log(this)
+    const image = this.src
+    const modalImg = document.querySelector('.modal__img')
+    const closeIcon = document.querySelector('.modal__icon-close')
+    const wrapper = document.querySelector('.wrapper')
+    modalImg.src = image
+    wrapper.classList.remove('hidden')
+    document.body.style.overflow = 'hidden'
+    closeIcon.addEventListener('click', ()=>{wrapper.classList.add('hidden'); document.body.style.overflow = 'auto'})
+    wrapper.addEventListener('click', ()=>{wrapper.classList.add('hidden');  document.body.style.overflow = 'auto'})
+
+
+
+}
+
+
